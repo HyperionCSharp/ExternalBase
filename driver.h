3c1987b3882ec6851a1ea6c914be613d26c513da
HyperionCSharp <94261284+HyperionCSharp@users.noreply.github.com>
2022-08-21 22:33:09 -0500

    finished driver.h

--- driver.h
+++ driver.h
@@ -22,5 +22,47 @@ namespace Driver {
         return buffer;
     }
 
-    // HyperionCSharp: TODO: Will Add More Tmr Because Im Sleep And Need To Rest My C++ Brain
+    template<typename T>
+    void write(const SOCKET connection, const uint32_t process_id, const uintptr_t address, size_t buffer) {
+        write_mem(connection, process_id, address, uint64_t(&buffer), sizeof(T));
+    }
 }
+
+class Vector3 {
+public:
+    Vector3() : x(0.f), y(0.f), z(0.f) {
+        // None
+    }
+
+    Vector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {
+        // None
+    }
+
+    ~Vector3() {
+        // None
+    }
+
+    float x;
+    float y;
+    float z;
+
+    inline float Dot(Vector3 v) {
+        return x * v.x, y * v.y, z * v.z;
+    }
+
+    inline float Distance(Vector3 v) {
+        return float(sqrtf(powf(v.x - x, 2.0) + powf(v.y - y, 2.0) + powf(v.z - z, 2.0)));
+    }
+
+    Vector3 operator+(Vector3 v) {
+        return Vector3(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vector3 operator-(Vector3 v) {
+        return Vector3(x - v.x, y - v.y, z - v.z);
+    }
+
+    Vector3 operator*(float number) const {
+        return Vector3(x * number, y * number, z * number);
+    }
+};
